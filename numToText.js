f92483e158c1f17cca33efbc58d64da46a64ffc8
Shams Ali <ali.s90@outlook.com>
2016-03-30 04:50:07 -0600

    numToText without inner function

--- numToText.js
+++ numToText.js
@@ -2,10 +2,31 @@
 
 Extra Credit: Have the function handle numbers of any digit size.
 
-Ex: numToText("I have 5 dogs and 6 ponies"); // returns "I have five dogs and six ponies"
+Ex: numToText("5I have 5 dogs and 6 ponies"); // returns "I have five dogs and six ponies"
 
 */
 
 var numToText = function(str) {
-
+    var objText = { 1: "one",
+                      2: "two",
+                      3: "three",
+                      4: "four",
+                      5: "five",
+                      6: "six",
+                      7: "seven",
+                      8: "eight",
+                      9: "nine" };
+    var res = '';
+    if(str.length === 0){
+      return res;
+    }else{
+      if(objText.hasOwnProperty(str[0])){
+          res += objText[str[0]];
+      }else{
+         res += str[0];
+      }
+      return res + numToText(str.slice(1));
+    }
 };
+
+console.log(numToText("5I have 5 dogs and 6 ponies"));
